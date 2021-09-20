520e023d34a6152789375d597c8b76d2ae90325e
Nortvest <HIKO22@list.ru>
2021-09-20 22:24:24 +0300

    fifth commit

--- GetWeather.py
+++ GetWeather.py
@@ -31,29 +31,38 @@ class GetWeather:
             suffix = f'{self.day + 1}-day/'
         return suffix
 
+    @staticmethod
+    def parser(url, suffix):
+        response = requests.get(url + suffix,
+                                headers={
+                                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'})
+        soup = BeautifulSoup(response.text, 'html.parser')
+        time = ['{:02d}:{}'.format(int(i.text[:-2]), i.text[-2:])
+                for i in soup.find('div', class_='widget__row_time').find_all('div', class_='widget__item')]
+        precipitation = [i.get('data-text')
+                         for i in soup.find('div', class_='widget__row_table').find_all('span', class_='tooltip')]
+        temperature = [i.text
+                       for i in soup.find('div', class_='widget__row_temperature').find_all('span',
+                                                                                            class_='unit unit_temperature_c')]
+        wind = [i.text.strip()
+                for i in
+                soup.find_all('div', class_='widget__row_table')[2].find_all('span', class_='unit unit_wind_m_s')]
+        per_cm = [i.text.strip()
+                  for i in soup.find('div', class_='widget__row_precipitation').find_all('div', class_='w_prec__value')]
+        return time, precipitation, temperature, wind, per_cm
+
     def get_weather(self):
         data = []
         suffix = self.__get_suffix()
         print(self.url + suffix)
-        response = requests.get(self.url + suffix,
-                                headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'})
-        soup = BeautifulSoup(response.text, 'html.parser')
-        data.append(['{:02d}:{}'.format(int(i.text[:-2]), i.text[-2:])
-                     for i in soup.find('div', class_='widget__row_time').find_all('div', class_='widget__item')])
-        data.append([i.get('data-text')
-                     for i in soup.find('div', class_='widget__row_table').find_all('span', class_='tooltip')])
-        data.append([i.text
-                     for i in soup.find('div', class_='widget__row_temperature').find_all('span', class_='unit unit_temperature_c')])
-        data.append([i.text.strip()
-                     for i in soup.find_all('div', class_='widget__row_table')[2].find_all('span', class_='unit unit_wind_m_s')])
-        data.append([i.text.strip()
-                     for i in
-                     soup.find('div', class_='widget__row_precipitation').find_all('div', class_='w_prec__value')])
-        self.set_table(data)
+        time, precipitation, temperature, wind, per_cm = self.parser(self.url, suffix)
+        for t1, p1, t2, w, p2 in zip(time, precipitation, temperature, wind, per_cm):
+            data.append([t1, p1, t2, w, p2])
+        self.set_table(data, ['Время', 'Небо', 't °С', 'Ветер', 'Осадки'])
 
     @staticmethod
-    def set_table(data):
-        print(tabulate(data, tablefmt="fancy_grid"))
+    def set_table(data, headers):
+        print(tabulate(data, headers, tablefmt="fancy_grid"))
 
 
 if __name__ == '__main__':
